1bab735a0215179759463128d78d405d14e5e2fa
abubakar-sadiq-001 <abubakarsadiqi532@gmail.com>
2026-01-02 15:45:58 +0100

    Fix import path

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 import { Link, useSearchParams } from "react-router";
-import { useSignup } from "../lib/useSignUp";
+import { useSignup } from "../lib/useSignup.js";
 
 function Signup() {
   const [searchParam, setSeachParam] = useSearchParams();
